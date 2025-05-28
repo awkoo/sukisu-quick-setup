ac2970fcbcb12e734df5baa4f68bfcf77e081b06
lamadaemon <i@lama.icu>
2025-05-27 20:13:20 -0700

    reformat log info

--- sukisu_setup.sh
+++ sukisu_setup.sh
@@ -35,86 +35,91 @@ SELF_DIR=$(realpath $(dirname $0))
 
 function check-exec() {
     if ! which $1 &> /dev/null; then
-        echo "missing $1! abort!"
+        log "missing $1! abort!"
         exit 1
     fi
+}
+
+function log() {
+    echo "[$(basename $0)] $@"
 }
 
 check-exec curl
 check-exec git
 check-exec jq
 
-echo "setting up $SUKISU_REPO"
+log "setting up $SUKISU_REPO"
 
 curl -LSs "https://raw.githubusercontent.com/$SUKISU_REPO/main/kernel/setup.sh" | bash -s $BRANCH
 
 if ! [ $? -eq 0 ]; then
-    echo "setup failed ($?)!"
+    log "setup failed ($?)!"
     exit 1
 fi
 
 
 cd KernelSU
-echo "enter KernelSU directory"
+log "enter KernelSU directory"
 if ! [ "$COMMIT" = "" ]; then
-    echo "using commit $COMMIT"
+    log "using commit $COMMIT"
     git reset --hard $COMMIT
 fi
 
 
 if [ "$APPLY_ZAKO_HASH" = "yes" ]; then
-    echo "adding zakosu manager"
+    log "adding zakosu manager"
     patch -p1 < $SELF_DIR/01-add-zakosu-manager.patch
 fi
 
 if [ "$SUSFS_DUPLICATE_DEF_FIX" = "yes" ]; then
-    echo "applying susfs duplicate definition fix"
+    log "applying susfs duplicate definition fix"
     patch -p1 < $SELF_DIR/03-fix-susfs-duplicate-definition.patch
 fi
 
 cd ..
 
-echo "downloading patch_linux..."
+log "downloading patch_linux..."
 $SELF_DIR/download_patch_linux.sh
 
-echo "adding CONFIG_KPM=y"
-echo "CONFIG_KPM=y" >> arch/arm64/configs/gki_defconfig
+log "adding CONFIG_KPM=y"
+log "CONFIG_KPM=y" >> arch/arm64/configs/gki_defconfig
 
-echo "adding auto kpm patch"
+log "adding auto kpm patch"
 if [ -f "$SELF_DIR/patch_kpm.sh" ]; then
     patch -p1 < $SELF_DIR/02-add-auto-kpm-patch.patch
 else
-    echo "error! patch_kpm.sh not found!"
+    log "error! patch_kpm.sh not found!"
     exit 1
 fi
 
-echo "adding susfs4ksu"
+log "adding susfs4ksu"
 if [ -f "$SELF_DIR/patch_susfs.sh" ]; then
     $SELF_DIR/patch_susfs.sh $SUSFS_BRANCH
 else
-    echo "error! patch_susfs.sh not found!"
+    log "error! patch_susfs.sh not found!"
     exit 1
 fi
 
-echo "applying custom kernel name settings"
+log "applying custom kernel name settings"
 if [ "$REMOVE_DIRTY" = "yes" ]; then
     sed -i -e "s/printf '%s' -dirty/:/g" scripts/setlocalversion
 fi
 
 
 if ! [ "$CUSTOM_KERNEL_SUFFIX" = "" ]; then
-    sed -i -e "s/echo \"\$res\"/echo \"\$res-$CUSTOM_KERNEL_SUFFIX\"/g" scripts/setlocalversion
+    sed -i -e "s/log \"\$res\"/log \"\$res-$CUSTOM_KERNEL_SUFFIX\"/g" scripts/setlocalversion
 fi
 
 if ! [ "$CUSTOM_KERNEL_NAME" = "" ]; then
     : > scripts/setlocalversion # :> owo
-    echo "#!/bin/sh" >> scripts/setlocalversion
-    echo "echo '$CUSTOM_KERNEL_NAME'" >> scripts/setlocalversion
+    log "#!/bin/sh" >> scripts/setlocalversion
+    log "log '$CUSTOM_KERNEL_NAME'" >> scripts/setlocalversion
 fi
 
 if [ "$APPLY_EXTRA_HIDE_PATCH" = "yes" ]; then
+    log "applying extra hide stuff"
     patch -p1 < $SELF_DIR/SukiSU_patch/69_hide_stuff.patch
 fi
 
-echo "setup done"
+log "setup done"
 
